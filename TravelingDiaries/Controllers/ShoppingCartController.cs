d1ecd9b2738b05e9f098786925cc69854e1f7626
vaibhav.bhatnagar2 <vaibhav.bhatnagar2@globallogic.com>
2022-08-23 06:16:04 +0530

    Add project files.

---
+++ TravelingDiaries/Controllers/ShoppingCartController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TravelingDiaries.Models;
+using TravelingDiaries.ViewModel;
+
+namespace TravelingDiaries.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        private readonly IPlaceRepository placeRepository;
+        private readonly ShoppingCart _shoppingCart;
+
+        public ShoppingCartController(IPlaceRepository placeRepository, ShoppingCart shoppingCart)
+        {
+            this.placeRepository = placeRepository;
+            _shoppingCart = shoppingCart;
+        }
+
+        public ViewResult Index()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            var shoppingCartViewModel = new ShoppingCartViewModel
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+
+            return View(shoppingCartViewModel);
+        }
+
+        public RedirectToActionResult AddToShoppingCart(int id)
+        {
+            var selectedPie = placeRepository.GetAllPlaces().FirstOrDefault(p => p.PlaceID == id);
+
+            if (selectedPie != null)
+            {
+                _shoppingCart.AddToCart(selectedPie, 1);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public RedirectToActionResult RemoveFromShoppingCart(int id)
+        {
+            var selectedPie = placeRepository.GetAllPlaces().FirstOrDefault(p => p.PlaceID == id);
+
+            if (selectedPie != null)
+            {
+                _shoppingCart.RemoveFromCart(selectedPie);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
